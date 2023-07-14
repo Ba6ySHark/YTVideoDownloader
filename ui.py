16a09d83e713475e65ea0823a80c92e10118e180
Stepan Susorov <137768644+Ba6ySHark@users.noreply.github.com>
2023-07-14 11:01:55 -0600

    Added new widgets

--- ui.py
+++ ui.py
@@ -52,7 +52,7 @@ class MyTabView(customtkinter.CTkTabview):
         self.show_file_select = customtkinter.CTkOptionMenu(master=self.top, values=["Yes", "No"], width=55, height=30, corner_radius=5)
         self.show_file_select.place(x=510, y=90)
         self.top.pack(padx=0, pady=0)
-        self.log_window = customtkinter.CTkTextbox(master=self.downloader_frame, width=570, height=425, corner_radius=15, state="disabled")
+        self.log_window = customtkinter.CTkTextbox(master=self.downloader_frame, width=570, height=425, corner_radius=15)
         self.log_window.pack(padx=0, pady=0, expand=True, fill="both")
         self.downloader_frame.pack(padx=10, pady=10, side=customtkinter.LEFT, fill="both")
         self.empty_space_right = customtkinter.CTkFrame(master=self.tab("Download"), fg_color="transparent")
@@ -61,12 +61,9 @@ class MyTabView(customtkinter.CTkTabview):
         #Videos page (2)
         self.playerFrame = customtkinter.CTkFrame(master=self.tab("Videos"), corner_radius=15) #width=850, height=580
         self.playerFrame.pack(padx=10, pady=10, fill="both", expand=True, side=customtkinter.LEFT)
-        #self.vidPlayer = TkinterVideo(master=self.playerFrame, scaled=True, keep_aspect=True, consistant_frame_rate=True, bg="black")
-        #self.vidPlayer.set_resampling_method(1)
         self.vlc_frame = customtkinter.CTkFrame(master=self.playerFrame)
         self.vlc_frame.pack(padx=20, pady=20, fill=customtkinter.BOTH, expand=True)
         self.vidPlayer = tkinter.Canvas(self.vlc_frame, bg="black", highlightthickness=0).pack(fill=tkinter.BOTH, expand=True)
-        #self.vidPlayer.pack(padx=20, pady=20, expand=True, fill="both")
         self.progress_slider = customtkinter.CTkSlider(master=self.playerFrame, from_=0, to=1000) #width=810
         self.progress_slider.set(-1)
         self.progress_slider.pack(fill="both", padx=20, pady=0)
@@ -80,14 +77,7 @@ class MyTabView(customtkinter.CTkTabview):
         self.now_playing.pack(padx=10, pady=10, expand=True, side=customtkinter.LEFT)
         self.video_list = ScrollFrame(self.tab("Videos"), width=340)
         lg.createVideoList()
-        self.video_list.pack(padx=10, pady=10, fill="both", side=customtkinter.LEFT)
-        
-        self.clear_button.configure(command=lg.clearEntry)
-        self.download_button.configure(command=lg.downloadVideo)
-        
-        self.progress_slider.configure(command=lg.seekSliderValue)
-        self.pause_button.configure(command=lg.playPause)
-        
+        self.video_list.pack(padx=10, pady=10, fill="both", side=customtkinter.LEFT) 
         
         #Music page (3)
         self.empty_space_left_new = customtkinter.CTkLabel(master=self.tab("Music"), text="", fg_color="transparent")
@@ -95,7 +85,8 @@ class MyTabView(customtkinter.CTkTabview):
         self.audPlayerFrame = customtkinter.CTkFrame(master=self.tab("Music"), width=480, corner_radius=15)
         self.audPlayer = customtkinter.CTkTextbox(master=self.audPlayerFrame, width=460, corner_radius=15)
         self.audPlayer.pack(padx=10, pady=10, expand=True, fill="both")
-        self.music_slider = customtkinter.CTkSlider(master=self.audPlayerFrame, width=460)
+        self.music_slider = customtkinter.CTkSlider(master=self.audPlayerFrame, width=460, from_=0, to=1000)
+        self.music_slider.set(-1)
         self.music_slider.pack(padx=0, pady=10)
         self.controls = customtkinter.CTkFrame(master=self.audPlayerFrame, width=460, fg_color="transparent")
         self.es1 = customtkinter.CTkLabel(master=self.controls, text="", fg_color="transparent")
@@ -117,6 +108,23 @@ class MyTabView(customtkinter.CTkTabview):
         self.audio_list.pack(padx=20, pady=20, fill="both", side=customtkinter.LEFT)
         self.empty_space_right_new = customtkinter.CTkLabel(master=self.tab("Music"), text="", fg_color="transparent")
         self.empty_space_right_new.pack(padx=10, pady=10, expand=True, side=customtkinter.LEFT)
+        
+        #Completed tasks page (4)
+        self.esn1 = customtkinter.CTkLabel(master=self.tab("Completed tasks"), text="", fg_color="transparent")
+        self.esn1.pack(expand=True, fill="both", side=customtkinter.LEFT)
+        self.logFrame = customtkinter.CTkScrollableFrame(master=self.tab("Completed tasks"), width=570, corner_radius=15)
+        lg.createLogList()
+        self.logFrame.pack(padx=10, pady=10, fill="both", side=customtkinter.LEFT)
+        self.esn2 = customtkinter.CTkLabel(master=self.tab("Completed tasks"), text="", fg_color="transparent")
+        self.esn2.pack(expand=True, fill="both", side=customtkinter.LEFT)
+        
+        self.clear_button.configure(command=lg.clearEntry)
+        self.download_button.configure(command=lg.downloadVideo)
+        self.progress_slider.configure(command=lg.seekSliderValue)
+        self.pause_button.configure(command=lg.playPause)
+        self.music_slider.configure(command=lg.seekMSliderValue)
+        self.stop_button.configure(command=lg.stopMp)
+        self.play_button.configure(command=lg.playMPause)
         
         
 class App(customtkinter.CTk):
