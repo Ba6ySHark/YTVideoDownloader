19dfc11e6a0b98f776cec7313ada9dc46ee2245b
Stepan Susorov <137768644+Ba6ySHark@users.noreply.github.com>
2023-07-16 12:44:27 -0600

    Download functions reworked

--- downloader.py
+++ downloader.py
@@ -1,7 +1,7 @@
 from database import DataBase
 from lyricsscraper import Scraper
 
-from pytube import YouTube, Playlist
+from pytube import YouTube
 import os
 import pathlib
 from moviepy.editor import *
@@ -16,20 +16,27 @@ class Loader:
     def update_url(self, new_url):
         self.video = YouTube(new_url)
         
-    def downloadVideo(self, resolution="144p"):
-        print(self.video.streams.filter(res=resolution, progressive=True))
+    def downloadVideo(self, resolution="144p", log=None):
+        #print(self.video.streams.filter(res=resolution, progressive=True))
         if len(self.video.streams.filter(res=resolution, progressive=True)) != 0:
-            self.video.streams.filter(res=resolution).first().download(self.videos_dir)
+            log.insert("end", "Recording of progressive type is found\nDownloading...\n")
+            tb = self.video.streams.filter(res=resolution).first().download(self.videos_dir)
+            log.insert("end", f"Savel file at {tb}\n")
+            log.insert("end", "Done\n")
         else:
+            log.insert("end", "There are no available recordings of progressive type found...\nDownloading video...\n")
             vid = self.video.streams.filter(res=resolution).first().download(f"temp/{self.videos_dir}").split("/")[-1]
-            print(vid)
+            #print(vid)
+            log.insert("end", "Downloading audio...\n")
             aud = self.downloadAudio(True)
             vclip = VideoFileClip(f"temp/{vid}")
             aclip = AudioFileClip(f"temp/{aud}")
             final = vclip.set_audio(aclip)
+            log.insert("end", "Merging...\n")
             final.write_videofile(os.path.join(self.videos_dir, f"{self.video.title}.mp4"), fps=30, threads=1, codec="libx264")
+            log.insert("end", "Done\n")
         
-    def downloadAudio(self, temp=False):
+    def downloadAudio(self, temp=False, log=None):
         downloaded_file = ""
         if temp:
             music_directory = f"temp/{self.music_dir}"
@@ -37,10 +44,12 @@ class Loader:
         else:
             music_directory = self.music_dir
             downloaded_file = self.video.streams.filter(only_audio=True).first().download(music_directory).split("/")[-1]
+            log.insert("end", f"Saved file at {downloaded_file}\n")
             os.rename(f"{downloaded_file}", (downloaded_file.split(".")[0] + ".mp3"))
             downloaded_file = downloaded_file.split(".")[0] + ".mp3"
             db = DataBase()
             data_len = db.getLength()
+            log.insert("end", f"Looking for {self.video.title} lyrics\n")
             lyrics = Scraper().get_lyrics(self.video.title, self.video.author)
             title = pathlib.PurePath(downloaded_file)
             #print(title.name)
@@ -48,5 +57,6 @@ class Loader:
             item = (data_len, f"{title.name}", f"{lyrics}", f"{self.video.author}")
             db.addItem(item)
             db.abort()
+            log.insert("end", "Done\n")
         return downloaded_file
     
--- downloaderThread.py
+++ downloaderThread.py
@@ -1,6 +1,7 @@
 import downloader
 
 from threading import Thread
+import webbrowser as wb
 import time
 import os
 import glob
@@ -19,22 +20,49 @@ class DThread(Thread):
         while True:
             
             if self.video_active:
-                downloader.Loader(self.link).downloadVideo(self.resolution)
+                self.logic.tabView.download_button.configure(state="disabled")
+                downloader.Loader(self.link).downloadVideo(self.resolution, self.logic.tabView.log_window)
                 self.logic.updateVideoList()
+                self.logic.tabView.log_window.insert("end", "Video list has been updated\n")
                 self.video_active = False
+                self.logic.tabView.download_button.configure(state="normal")
+                self.logic.tabView.log_window.insert("end", "Successfully downloaded a file!\n\n")
+                self.logic.clearEntry()
+                
+                if self.logic.tabView.show_file_select.get() == "Yes":
+                    wb.open("videos")
+                
+                with open("logs.txt", "a") as file:
+                    activity = self.logic.tabView.log_window.get("0.0", "end")
+                    print(activity)
+                    file.write(activity)
+                    self.logic.updateLogList(activity)
                 
                 temp_video_files = glob.glob("temp/videos/*")
                 for video in temp_video_files:
-                    print(video)
+                    #print(video)
                     os.remove(video)
                 temp_audio_files = glob.glob("temp/music/*")
                 for audio in temp_audio_files:
                     os.remove(audio)
             
             elif self.audio_active:
-                downloader.Loader(self.link).downloadAudio(temp=False)
+                self.logic.tabView.download_button.configure(state="disabled")
+                downloader.Loader(self.link).downloadAudio(temp=False, log=self.logic.tabView.log_window)
                 self.logic.updateAudioList()
+                self.logic.tabView.log_window.insert("end", "Music list has been updated\n")
                 self.audio_active = False
+                self.logic.tabView.download_button.configure(state="normal")
+                self.logic.tabView.log_window.insert("end", "Successfully downloaded a file!\n\n")
+                self.logic.clearEntry()
+                
+                if self.logic.tabView.show_file_select.get() == "Yes":
+                    wb.open("music")
+                
+                with open("logs.txt", "a") as file:
+                    activity = self.logic.tabView.log_window.get("0.0", "end")
+                    file.write(activity)
+                    self.logic.updateLogList(activity)
             
             time.sleep(10)
             
